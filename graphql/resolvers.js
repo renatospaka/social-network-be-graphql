cfabfb97f818dc79130218cf02f8b1615ff98615
Renato Spakauskas <renatospaka@gmail.com>
2021-01-23 17:01:37 -0300

    feat: :sparkles: get all posts

--- graphql/resolvers.js
+++ graphql/resolvers.js
@@ -122,5 +122,28 @@ module.exports = {
       createdAt: createdPost.createdAt.toISOString(),
       updatedAt: createdPost.updatedAt.toISOString()
     }
+  },
+
+  getPosts: async function(args, req) {
+    if (!req.isAuth) {
+      const error = new Error('User not authenticated.');
+      error.code = 401;
+      throw error;
+    }
+
+    const totalPosts = await Post.find().countDocuments();
+    const posts = await Post.find()
+      .sort({ createdAt: -1 })
+      .populate('creator');
+    return { 
+      posts: posts.map(p => {
+        return {
+          ...p._doc,
+          _id: p._id.toString(),
+          createdAt: p.createdAt.toISOString(),
+          updatedAt: p.updatedAt.toISOString()
+        }
+      }), 
+       totalPosts: totalPosts};
   }
 }
